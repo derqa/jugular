12c4025729d9a24117467e97c6cd8b692bc796c2
derqa <57926174+derqa@users.noreply.github.com>
2020-08-22 03:13:09 -0700

    First Commit
    Jugular fivem addon

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # jugular
 jugular stand-alone, fivem
+
+Converted from diamond pack @https://forum.cfx.re/t/release-diamond-casino-heist-vehicles/916569 For those that might like individual cars to make a custom pack from. This is my first release, so be gentle! Lol, needs some fat trimmed off and I will update it as I do so. 
+
+
+# To Install
+
+Add jugular to vehicle resource location. Add start jugular to server.cfg. All tuning and sounds are working. 
+
+
+-DerqaTarzan
---
+++ jugular/__resource.lua
@@ -0,0 +1,43 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+	'vehiclelayouts.meta',
+	'handling.meta',
+	'vehicles.meta',
+	'carcols.meta',
+	'carvariations.meta',
+	'audio/dlcvinewood_amp.dat10',
+	'audio/dlcvinewood_amp.dat10.nametable',
+	'audio/dlcvinewood_amp.dat10.rel',
+	'audio/dlcvinewood_game.dat151',
+	'audio/dlcvinewood_game.dat151.nametable',
+	'audio/dlcvinewood_game.dat151.rel',
+	'audio/dlcvinewood_mix.dat15',
+	'audio/dlcvinewood_mix.dat15.nametable',
+	'audio/dlcvinewood_mix.dat15.rel',
+	'audio/dlcvinewood_sounds.dat54',
+	'audio/dlcvinewood_sounds.dat54.nametable',
+	'audio/dlcvinewood_sounds.dat54.rel',
+	'audio/dlcvinewood_speech.dat4',
+	'audio/dlcvinewood_speech.dat4.nametable',
+	'audio/dlcvinewood_speech.dat4.rel',
+	'audio/sfx/dlc_vinewood/jugular.awc',
+	'audio/sfx/dlc_vinewood/jugular_npc.awc'
+	
+}
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+data_file 'AUDIO_GAMEDATA' 'audio/dlcvinewood_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/dlcvinewood_sounds.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'audio/dlcvinewood_mix.dat'
+data_file 'AUDIO_SYNTHDATA' 'audio/dlcVinewood_amp.dat'
+data_file 'AUDIO_SPEECHDATA' 'audio/dlcvinewood_speech.dat'
+
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_vinewood'
+
+client_script 'vehicle_names.lua'
